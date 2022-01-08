0f802b7692094832138853c38e1a19d6edc00039
Wpaswan <dablumcapaswan@gmail.com>
2022-01-07 23:49:06 -0800

    [I modified the code to add new employee]

--- EmpDBTest/UnitTest1.cs
+++ EmpDBTest/UnitTest1.cs
@@ -43,9 +43,26 @@ namespace EmpDBTest
                 System.Console.WriteLine("id: "+item.id+"Name: "+item.name+"Salary: "+item.Salary);
             }
         }
+        [TestMethod]
+        public void givenEmployee_OnPost_ShouldReturnAddedEmployee()
+        {
+            RestRequest request = new RestRequest("/employees", Method.POST);
+            JObject jObjectbody = new JObject();
+            jObjectbody.Add("name", "Clark");
+            jObjectbody.Add("Salary", "15000");
+            request.AddParameter("application/json", jObjectbody, ParameterType.RequestBody);
+
+            //act
+            IRestResponse response = client.Execute(request);
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.Created);
+            Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("Clark", dataResponse.name);
+            Assert.AreEqual(15000, dataResponse.Salary);
+
+        }
 
 
-       
+
     }
 
 
